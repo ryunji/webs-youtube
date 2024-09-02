de7175621849c3c753d387912dbcc50d0e4a0034
ryunji1990@gmail.com <ryunji1990@gmail.com>
2024-09-02 16:09:02 +0900

    변경 된 소스 커밋

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// index.js가 바로 index.html의 root에 넣어주는 역할을 하는 곳.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App />             {/* 여기는  App.js의 내용을 불러오는 부분이다. */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

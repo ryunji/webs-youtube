ace57b51f40b9a12910e39dd8888bbb6f22c4391
ryunji1990@gmail.com <ryunji1990@gmail.com>
2024-09-03 00:52:27 +0900

    04 SCSS 세팅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// ./ 은 root를 의미하고 src가 루트 이다.
+import './assets/scss/style.scss'
 
 // index.js가 바로 index.html의 root에 넣어주는 역할을 하는 곳.
 const root = ReactDOM.createRoot(document.getElementById('root'));

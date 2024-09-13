2de317c325284bc9b89413c064d7d70f8ba837bb
ryunji1990@gmail.com <ryunji1990@gmail.com>
2024-09-13 17:17:25 +0900

    컴퍼넌트 비동기 작업

--- src/components/section/Main.jsx
+++ src/components/section/Main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
 
-const Main = () => {
+// props는 App.js로부터 전달받은 속성을 전달해주는 것.
+const Main = (props) => {
   return (
-    <main id="main" role="main">
-      main
-    </main>
+    //셋(header, main, footer)을 묶는 부모 태그 필요 : div
+    <>
+      <Header/>
+      <main id="main" role="main">
+        {props.children}
+      </main>
+      <Footer/>
+    </>
   )
 }
 
--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -1,10 +1,13 @@
 //JSX는 자바스크립트와 XML 그리고 HTML을 합쳐놓은 문법.
 
 import React from 'react'
+import Main from '../components/section/Main'
 
 const Developer = () => {
   return (
-    <div>Developer</div>
+    <Main>
+      Developer
+    </Main>
   )
 }
 
